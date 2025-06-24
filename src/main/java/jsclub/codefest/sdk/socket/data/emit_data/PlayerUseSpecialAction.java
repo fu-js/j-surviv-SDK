926fbef7728606abac3215a7591fc2b1076e2f3f
DevPhiCap <phi.np.1953@aptechlearning.edu.vn>
2025-06-24 19:45:10 +0700

    update use special

--- src/main/java/jsclub/codefest/sdk/socket/data/emit_data/PlayerUseSpecialAction.java
+++ src/main/java/jsclub/codefest/sdk/socket/data/emit_data/PlayerUseSpecialAction.java
@@ -6,11 +6,8 @@ import jsclub.codefest.sdk.model.weapon.Weapon;
 public class PlayerUseSpecialAction {
     @SerializedName("direction")
     private String direction;
-    @SerializedName("attributes")
-    private Object special;
 
-    public PlayerUseSpecialAction(String direction, Weapon special) {
+    public PlayerUseSpecialAction(String direction) {
         this.direction = direction;
-        this.special = special;
     }
 }
