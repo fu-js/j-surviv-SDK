462a753aa08e25d7fce78080e775929d413f0ee8
Kazelf <166615901+Kazelf@users.noreply.github.com>
2025-06-27 14:56:34 +0700

    remove/sout mapData

--- src/main/java/jsclub/codefest/sdk/model/GameMap.java
+++ src/main/java/jsclub/codefest/sdk/model/GameMap.java
@@ -108,7 +108,7 @@ public class GameMap {
             String message = MsgPackUtil.decode(arg);
 //            System.out.println("MESSAGE UPDATE: " + message);
             MapData mapData = gson.fromJson(message, MapData.class);
-            System.out.println(mapData);
+            // System.out.println(mapData);
             List<Obstacle> newListObstacles = new ArrayList<>();
             List<Obstacle> newListChest = new ArrayList<>();
             List<Obstacle> newListTrap = new ArrayList<>();
