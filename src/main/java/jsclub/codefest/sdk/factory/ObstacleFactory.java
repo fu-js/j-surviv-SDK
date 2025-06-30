ea055107579aef3ff553b940fcccb1d08600974a
unknown <Tuantumat203@gmail.com>
2025-07-01 00:44:43 +0700

    Bỏ Ổ gai

--- src/main/java/jsclub/codefest/sdk/factory/ObstacleFactory.java
+++ src/main/java/jsclub/codefest/sdk/factory/ObstacleFactory.java
@@ -15,7 +15,6 @@ public class ObstacleFactory {
         Map.entry("CHEST", new Obstacle("CHEST", ElementType.CHEST, List.of(ObstacleTag.DESTRUCTIBLE, ObstacleTag.PULLABLE_ROPE, ObstacleTag.HERO_HIT_BY_BAT_WILL_BE_STUNNED), 20)),
         Map.entry("DRAGON_EGG", new Obstacle("DRAGON_EGG", ElementType.CHEST, List.of(ObstacleTag.DESTRUCTIBLE, ObstacleTag.PULLABLE_ROPE, ObstacleTag.HERO_HIT_BY_BAT_WILL_BE_STUNNED),50)),
         Map.entry("HUNT_TRAP", new Obstacle("HUNT_TRAP", ElementType.TRAP, List.of(ObstacleTag.TRAP, ObstacleTag.CAN_GO_THROUGH, ObstacleTag.CAN_SHOOT_THROUGH), 0)),
-        Map.entry("SPIKES", new Obstacle("SPIKES", ElementType.TRAP, List.of(ObstacleTag.TRAP, ObstacleTag.CAN_GO_THROUGH, ObstacleTag.CAN_SHOOT_THROUGH), 0)),
         Map.entry("BANANA_PEEL", new Obstacle("BANANA_PEEL", ElementType.TRAP, List.of(ObstacleTag.TRAP, ObstacleTag.CAN_GO_THROUGH, ObstacleTag.CAN_SHOOT_THROUGH) , 0)),
         Map.entry("INDESTRUCTIBLE", new Obstacle("INDESTRUCTIBLE", ElementType.INDESTRUCTIBLE, List.of(ObstacleTag.PULLABLE_ROPE, ObstacleTag.HERO_HIT_BY_BAT_WILL_BE_STUNNED))),
         Map.entry("POND", new Obstacle("POND", ElementType.INDESTRUCTIBLE, List.of(ObstacleTag.CAN_GO_THROUGH, ObstacleTag.CAN_SHOOT_THROUGH, ObstacleTag.HERO_HIT_BY_BAT_WILL_BE_STUNNED))),
