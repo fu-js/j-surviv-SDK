b67bb654a608d91ec397a551c2af6a985ff62509
unknown <Tuantumat203@gmail.com>
2025-07-03 22:02:20 +0700

    Fix Speed Gun

--- src/main/java/jsclub/codefest/sdk/factory/WeaponFactory.java
+++ src/main/java/jsclub/codefest/sdk/factory/WeaponFactory.java
@@ -20,9 +20,9 @@ public class WeaponFactory {
             
             // Gun Weapons
             Map.entry("SCEPTER", new Weapon("SCEPTER", ElementType.GUN, 30, 20, 3, 20, new int[]{1, 12}, -1, 12, 10)),
-            Map.entry("CROSSBOW", new Weapon("CROSSBOW", ElementType.GUN, 30, 30, 4, 30, new int[]{1, 8}, -1, 6, 6)),
-            Map.entry("RUBBER_GUN", new Weapon("RUBBER_GUN", ElementType.GUN, 40, 15, 2, 15, new int[]{1, 6}, -1, 6, 8)),
-            Map.entry("SHOTGUN", new Weapon("SHOTGUN", ElementType.GUN, 20, 45, 8, 45, new int[]{1, 2}, -1, 12, 4)),
+            Map.entry("CROSSBOW", new Weapon("CROSSBOW", ElementType.GUN, 30, 30, 4, 30, new int[]{1, 8}, -1, 8, 6)),
+            Map.entry("RUBBER_GUN", new Weapon("RUBBER_GUN", ElementType.GUN, 40, 15, 2, 15, new int[]{1, 6}, -1, 8, 8)),
+            Map.entry("SHOTGUN", new Weapon("SHOTGUN", ElementType.GUN, 20, 45, 8, 45, new int[]{1, 2}, -1, 16, 4)),
             
             // Throwable Weapons
             Map.entry("BANANA", new Weapon("BANANA", ElementType.THROWABLE, 35, 30, 4, 30, new int[]{1, 6}, 3, 3, 2)),
